138c3b312f3b3a6f9c3c8ba32f7f3018d3f8c35a
husneara <husnearaa99@gmail.com>
2023-12-02 13:01:05 +0600

    had done footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,9 +1,83 @@
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 
 const Footer = () => {
     return (
         <div>
-            
+            <footer className="px-4 divide-y bg-[#262626] text-gray-100 mt-10">
+	<div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+		<div className="lg:w-1/3">
+			<a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
+				<div className="flex items-center justify-center w-12 h-12 rounded-full bg-violet-400">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="flex-shrink-0 w-5 h-5 rounded-full text-gray-900">
+						<path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
+					</svg>
+				</div>
+				{/* <span className="self-center text-2xl font-semibold">Brand name</span> */}
+                <h2 className="text-3xl font-extrabold dark:text-white text-white"><span className="text-[#fed700] ">Tech</span>DiscoverHub</h2>
+			</a>
+		</div>
+		<div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
+			<div className="space-y-3">
+				<h3 className="tracki uppercase text-gray-50">Product</h3>
+				<ul className="space-y-1">
+					<li>
+						<a rel="noopener noreferrer" href="#">Features</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="#">Integrations</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="#">Pricing</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="#">FAQ</a>
+					</li>
+				</ul>
+			</div>
+			<div className="space-y-3">
+				<h3 className="tracki uppercase text-gray-50">Company</h3>
+				<ul className="space-y-1">
+					<li>
+						<a rel="noopener noreferrer" href="#">Privacy</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="#">Terms of Service</a>
+					</li>
+				</ul>
+			</div>
+			<div className="space-y-3">
+				<h3 className="uppercase text-gray-50">Developers</h3>
+				<ul className="space-y-1">
+					<li>
+						<a rel="noopener noreferrer" href="#">Public API</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="#">Documentation</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="#">Guides</a>
+					</li>
+				</ul>
+			</div>
+			<div className="space-y-3">
+				<div className="uppercase text-gray-50">Social media</div>
+				<div className="flex justify-start space-x-3">
+					<a rel="noopener noreferrer" href="#" title="Facebook" className="flex items-center p-1">
+						<FaFacebook className="text-3xl"></FaFacebook>
+					</a>
+					<a rel="noopener noreferrer" href="#" title="Twitter" className="flex items-center p-1">
+						<FaYoutube className="text-4xl"></FaYoutube>
+					</a>
+					<a rel="noopener noreferrer" href="#" title="Instagram" className="flex items-center p-1">
+						<FaInstagram className="text-3xl"></FaInstagram>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div className="py-6 text-sm text-center text-gray-400">© 2023 Company Co. All rights reserved.</div>
+</footer>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import Navbar from "../../components/Shared/Navbar/Navbar";
-
+import Footer from "../../components/Shared/Footer/Footer"
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Footer></Footer>
         </div>
     );
 };
