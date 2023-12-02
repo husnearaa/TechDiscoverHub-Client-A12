ca4c99c81e85e3af36557489ebed39746c2b7fb8
husneara <husnearaa99@gmail.com>
2023-12-02 15:49:11 +0600

    had done banner part

--- src/components/Shared/Banner/Banner.jsx
+++ src/components/Shared/Banner/Banner.jsx
@@ -1,9 +1,19 @@
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Banner = () => {
     return (
-        <div>
-            
-        </div>
+        <Carousel className='text-center' >
+            <div className=" h-[80vh]">
+                <img src={"https://i.ibb.co/k0KytNN/END-SEASON.jpg"} />
+            </div>
+            <div className=" h-[80vh]">
+                <img src={"https://i.ibb.co/dprJdfK/Brand-new-Smartwatch.jpg"} />
+            </div>
+            <div className=" h-[80vh]">
+                <img src={"https://i.ibb.co/dtD5Fh4/BLUETOOTH-SPEKER.jpg"} />
+            </div>
+        </Carousel>
     );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../../components/Shared/Navbar/Navbar";
-import Footer from "../../components/Shared/Footer/Footer"
+import Footer from "../../components/Shared/Footer/Footer";
+import Banner from "../../components/Shared/Banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner></Banner>
             <Footer></Footer>
         </div>
     );
