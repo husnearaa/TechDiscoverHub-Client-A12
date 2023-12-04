d4635ef3ffa4bc5c103bc3d6b4e628b381b63ed5
husneara <husnearaa99@gmail.com>
2023-12-04 14:31:33 +0600

    has completed the private route

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -18,13 +18,13 @@ const Products = () => {
                 <div className="mx-auto md:w-3/12 ">
                     <h2 className="text-4xl font-extrabold text-center py-10 border-b-2 border-black">Products</h2>
                 </div>
-               <div className="mx-auto md:w-4/6">
-               <p className="text-center font-medium py-6 text-sm md:text-base">Explore Our Diverse Range of High-Quality Products.
-                    Experience Quality and Style in Every Purchase.
-                    Browse, Click, Enjoy – Your Perfect Product Awaits.
-                    Elevate Your Lifestyle with Our Curated Selection.Shop with Confidence.
-                </p>
-               </div>
+                <div className="mx-auto md:w-4/6">
+                    <p className="text-center font-medium py-6 text-sm md:text-base">Explore Our Diverse Range of High-Quality Products.
+                        Experience Quality and Style in Every Purchase.
+                        Browse, Click, Enjoy – Your Perfect Product Awaits.
+                        Elevate Your Lifestyle with Our Curated Selection.Shop with Confidence.
+                    </p>
+                </div>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 pt-10'>
                 {products.map(({ _id, image, product_name, tags }) => (
